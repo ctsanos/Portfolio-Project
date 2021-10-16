c1fb602baa96995609b76155053bb1c55c73c28c
Christopher Tsanos <ctsanos@gmail.com>
2021-10-16 08:54:39 -0400

    vehicle check in form omplete

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function(){
+   
+    // $(".btn").on("click", function(){
+    //     if ($(this).hasClass(".active")){
+    //         $(this).addClass('.btn-primary');
+    //         $(this).addClass('.selected');            
+    //     }else{
+    //         $(this).removeClass(".focus").removeClass('.btn-primary');
+    //     }
+        
+    // });
+
+
+});
